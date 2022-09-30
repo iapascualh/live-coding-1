527ea07fd5f00de253c0a665ec18b6df4d3a9141
Irving Alexis Pascual Hernandez <hpascual117gmail.com>
2022-09-30 00:06:50 -0500

    chore | se concreto el ejercio correctamente

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-// Escribe tu código aquí.
+const myArray = ["insecto", "bootcamp", "mango", "reptil", "mosca", "escritorio"];
+let arraypalabras=bigWords("bocina", myArray);
+palabra=document.getElementById("listado");
+output(arraypalabras);
+    function output(arreglo2){
+        // (inicializacion;      condicion;              in/decremento)
+    for (let contador = 0; contador < arreglo2.length; contador++) {
+        const element = arreglo2[contador];
+        palabra.innerHTML+=`<li id="lista">${arreglo2[contador]}</li>`
+    };//for    
+    };//imprimali
+    function bigWords(np,arreglo1){
+    palabra=[];
+        // (inicializacion;      condicion;              in/decremento)
+    for (let contador = 0; contador < arreglo1.length; contador++) {
+        const element = arreglo1[contador];
+        if(np.length<arreglo1[contador].length)
+        {
+            palabra.push(arreglo1[contador]);
+        }//if
+    }//for
+    return palabra;
+    };
+
